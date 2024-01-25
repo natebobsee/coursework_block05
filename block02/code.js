e93f4d850748bd01eb486635fdaae4bd4ec40f32
natebobsee <nate.bob.see@gmail.com>
2024-01-24 16:15:10 -0800

    project block05

---
+++ block02/code.js
@@ -0,0 +1,8 @@
+console.log("Hello Fullstack!")
+const myheading = document.querySelector("h1")
+myheading.textContent = "hello world";
+//document.querySelector("html").addEventListener("click",function () {
+  //  alert("ouch stop poling me");
+//})
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
